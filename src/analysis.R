9b21d2dc8d010f62cfaa7851e1344a1f4a833d1a
Jeffrey K Naecker <jnaecker@gmail.com>
2017-08-10 16:35:02 -0400

    Fixed bug in question detection

--- src/analysis.R
+++ src/analysis.R
@@ -68,26 +68,29 @@ enrollments %>%
 # import pdf data
 instructor_first_name <- "Jeffrey"
 instructor_last_name <- "Naecker"
+master_string <- "(\\W{6,8}[0-9]{1,2}\\..*)|(\\W{16,16}.*\n\\W{20,})"
 
 extract_response_data <- function(response_text){
   response <- response_text %>% 
     str_replace_all("\nCopyright Wesleyan University\\W*[0-9]{1,2}/[0-9]{1,2}\n", "") %>% # remove headers
-    str_replace_all(paste(instructor_last_name, instructor_first_name, "\\([A-Z]{4,4} [0-9]{3}\\)\n"), "") # remove footers
+    str_replace_all(paste(instructor_last_name, ", ", instructor_first_name, " \\(\\s?[A-Z]{3,4} [0-9]{3}\\)", sep = ""), "") # remove footers
   
   questions <- response %>%
-    str_extract_all("(\\W{6,8}[0-9]{1,2}\\..*)|(\\W{16,16}.*\n\\W{18,})") %>%
+    str_extract_all(master_string) %>%
     unlist() %>%
     str_replace_all("[0-9]{1,2}\\. ", "") %>% # remove qestion numbers
     str_replace_all("^(\"|\\.|!|\\])", "") %>% # remove misc punctuation at beginning of line
     str_replace_all("\\[$", "") %>%           # remove misc punctuation at end of line
+    str_replace_all("\\.\\\n", "") %>%           # random fix
     str_trim()
   
   answers <- response %>%
-    str_split("(\\W{6,8}[0-9]{1,2}\\..*)|(\\W{16,16}.*\n\\W{18,})") %>%
+    str_split(master_string) %>%
     unlist() %>%
     str_replace_all("Copyright Wesleyan University\\W*[0-9]{1,2}/[0-9]{1,2}", "") %>% # clean up page break
+    str_replace_all("\n", "") %>% #remove line breaks
     str_trim() %>%
-    gsub("([0-9])-\\w.*$", "\\1", .)
+    gsub("([0-9])-\\w.*$", "\\1", .) # remove text from numerical responses
   
   data.frame(
     questions = questions,
@@ -96,47 +99,57 @@ extract_response_data <- function(response_text){
     subset(answers != "")
 }
 
+extract_evaluation_data <- function(document_name) {
+  text <- pdf_text(file.path(evaluations_data_path, document_name))[1]
+  data_frame(
+    semester = str_extract(text, "Fall|Spring"),
+    year = str_extract(text, "20[0-9]{2,2}"),
+    department = str_extract(text, "[A-Z]{3,4}"),
+    course = str_match(text, "([A-Z]{3,4}) ([0-9]{3,3})")[,3],
+    audience = str_match(text, "(Project Audience) ([0-9]+)")[,3],
+    responses_received = str_match(text, "(Responses Received) ([0-9]+)")[,3]
+    )
+}
+
 extract_responses <- function(document_name) {
-  pdf_text(file.path(evaluations_data_path, document_name)) %>%
+  responses <- pdf_text(file.path(evaluations_data_path, document_name)) %>%
     paste(., collapse = "") %>% 
     str_split("Course Evaluation Form( \\(continued\\))\n", ) %>%
     unlist()
+  data.frame(
+    response_number = c(1:length(responses)),
+    responses = responses
+  )
 }
 
-evaluations <- data.frame(
+evaluations <- data_frame(
   document_name = list.files(path = evaluations_data_path, pattern = ".*Response Sheet.*.pdf")) %>%
   mutate(
-    responses = map(document_name, ~ extract_responses(.))  
+    evaluation_data = map(document_name, ~ extract_evaluation_data(.))
   ) %>% 
-  unnest() %>%
+  unnest()
+  
+
+responses <- evaluations %>% 
+  mutate(
+    responses = map(document_name, ~ extract_responses(.))  
+  ) %>% unnest
+
+responses_long <- responses %>%
   mutate(
     response_data = map(responses, ~ extract_response_data(.))
-  )
+  ) %>%
+  unnest()
+
+responses_wide <- 
+  responses_long %>% 
+  dcast(semester + year + department + course + response_number ~ questions, value.var = "answers") 
+
+## make variables have right class
 
 
 
-
-
-response <- course[[1]][2] %>%
-
-
-
-
-
-responses <- do.call("rbind", out) 
-
-
-responses_long <- 
-  responses %>% 
-  dcast(id ~ questions, value.var = "answers") 
-
-varlist <- c("I knew what was expected of me in this course.","Instructor communicated knowledge effectively.","Instructor conveyed enthusiasm for the subject.","Instructor treated students with respect.","Instructor was accessible outside of class.","Instructor was readily available to answer questions outside of class","My knowledge and understanding of the subject matter have increased as a result of this course","My understanding/ skills grew as a result of this course.","Rate your level of effort in this course:","The assignments reinforced my understanding of the course material","The assignments were a useful part of the course.","The Course","The instructor added value to the course beyond what I could get from simply doing the readings","The instructor was successful at facilitating interaction in the classroom","The Teaching","There was a clear connection between instruction and assessment.","This course changed the way that I think about the world", "On average, how many hours per week did you spend on coursework outside of class?")
-for (var in varlist) {
-  responses_long[, var] <- as.numeric(responses_long[, var])
-}
-
-responses_long$`What is your expected grade in this course?` <- factor(responses_long$`What is your expected grade in this course?`, levels = c("A+ or A", "A- or B+", "B or B-"))
-
+#### PLOTS ####
 
 ggplot(aes(x=`The Course`, y=`The Teaching`), data = responses_long) + geom_jitter(width=.1, height=.1) + stat_smooth(method = "lm")
 
